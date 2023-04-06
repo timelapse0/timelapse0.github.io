894bada6f8b53965c30e464b972f76b31806c9d3
timelapse0 <turtletortoisesoup@outlook.com>
2023-04-06 17:00:15 +0800

    Site updated: 2023-04-06 16:59:46

--- js/my.js
+++ js/my.js
@@ -5,7 +5,9 @@ $('css').each(function(){
     var html = $(this).html();
     $(this).html('<pre><code class=\'language-css\'>'+html+'</code></pre>');
     })
-    $('js').each(function(){
+$('js').each(function(){
     var html = $(this).html();
     $(this).html('<pre><code class=\'language-js\'>'+html+'</code></pre>');
 })
+
+
